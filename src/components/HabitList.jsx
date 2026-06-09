ddac8e6194e49a5c02f8e31117f02febf13a5829
Shreya Mummigatti <smummigatti2000@gmail.com>
2026-06-09 18:03:07 +0530

    habit

--- src/components/HabitList.jsx
+++ src/components/HabitList.jsx
@@ -1,27 +1,32 @@
+import { useState, useEffect } from "react";
+
 export default function HabitList() {
+  const [habits, setHabits] = useState([]);
+
+  useEffect(() => {
+    const savedHabits = JSON.parse(localStorage.getItem("habits")) || [];
+
+    setHabits(savedHabits);
+  }, []);
+
   return (
     <div className="glass-card">
       <h2>Today's Habits</h2>
 
-      <div className="habit-row">
-        <input type="checkbox" defaultChecked />
-        Java Practice
-      </div>
-
-      <div className="habit-row">
-        <input type="checkbox" defaultChecked />
-        Drink Water
-      </div>
-
-      <div className="habit-row">
-        <input type="checkbox" />
-        Exercise
-      </div>
-
-      <div className="habit-row">
-        <input type="checkbox" defaultChecked />
-        Meditation
-      </div>
+      {habits.length === 0 ? (
+        <p>No habits added yet.</p>
+      ) : (
+        habits.map((habit, index) => (
+          <div
+            key={index}
+            style={{
+              marginBottom: "10px",
+            }}
+          >
+            {habit.completed ? "✅" : "⬜"} {habit.name}
+          </div>
+        ))
+      )}
     </div>
   );
 }
