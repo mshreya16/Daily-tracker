ba273dbaffdf53a7dbefa554dbfb9b7a1ea0564d
Shreya Mummigatti <smummigatti2000@gmail.com>
2026-06-09 19:15:49 +0530

    weeklt chart

--- src/components/WeeklyChart.jsx
+++ src/components/WeeklyChart.jsx
@@ -7,27 +7,47 @@ import {
   Tooltip,
 } from "recharts";
 
-const data = [
-  { day: "Mon", score: 65 },
-  { day: "Tue", score: 80 },
-  { day: "Wed", score: 72 },
-  { day: "Thu", score: 90 },
-  { day: "Fri", score: 78 },
-  { day: "Sat", score: 95 },
-  { day: "Sun", score: 88 },
-];
-
 export default function WeeklyChart() {
+  const activityDates = JSON.parse(localStorage.getItem("activityDates")) || [];
+
+  const days = [];
+
+  const today = new Date();
+
+  for (let i = 6; i >= 0; i--) {
+    const date = new Date();
+
+    date.setDate(today.getDate() - i);
+
+    const formatted = date.toISOString().split("T")[0];
+
+    const dayName = date.toLocaleDateString("en-US", {
+      weekday: "short",
+    });
+
+    days.push({
+      day: dayName,
+      score: activityDates.includes(formatted) ? 100 : 0,
+    });
+  }
+
   return (
     <div className="glass-card">
-      <h2>Weekly Progress</h2>
+      <h2>Weekly Activity</h2>
 
-      <div style={{ height: 250 }}>
+      <div
+        style={{
+          height: 250,
+        }}
+      >
         <ResponsiveContainer width="100%" height={250}>
-          <LineChart data={data}>
+          <LineChart data={days}>
             <XAxis dataKey="day" />
-            <YAxis />
+
+            <YAxis domain={[0, 100]} />
+
             <Tooltip />
+
             <Line
               type="monotone"
               dataKey="score"
